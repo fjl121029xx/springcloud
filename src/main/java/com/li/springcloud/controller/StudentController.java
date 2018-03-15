72f7ef9d010d961f7a9e357132b279452f3aff6c
fjl121029xx <sishengqikuo_xx@163.com>
2018-03-15 17:42:34 +0800

    spring cloud微服务实战 : 服务提供者和服务消费者

--- src/main/java/com/li/springcloud/controller/StudentController.java
+++ src/main/java/com/li/springcloud/controller/StudentController.java
@@ -3,6 +3,8 @@ package com.li.springcloud.controller;
 import com.li.springcloud.dao.StudentMapper;
 import com.li.springcloud.entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,6 +14,14 @@ import java.util.List;
 
 @Controller
 public class StudentController {
+    @Autowired
+    private RestTemplateBuilder builder;
+
+    // 使用RestTemplateBuilder来实例化RestTemplate对象，spring默认已经注入了RestTemplateBuilder实例
+    @Bean
+    public RestTemplate restTemplate() {
+        return builder.build();
+    }
 
     @Autowired
     private RestTemplate restTemplate;
