f93ec05e9df4d5b2e1e351660beb56a880d5f4ef
fjl121029xx <sishengqikuo_xx@163.com>
2018-03-16 14:18:00 +0800

    spring cloud微服务实战 : 服务提供者和服务消费者(2018-03-16 没啥用的改动)

--- src/main/java/com/li/springcloud/SpringcloudApplication.java
+++ src/main/java/com/li/springcloud/SpringcloudApplication.java
@@ -1,13 +1,8 @@
 package com.li.springcloud;
 
-import com.li.springcloud.dao.StudentMapper;
-import com.li.springcloud.entity.Student;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import java.util.List;
 
 @SpringBootApplication
 @MapperScan("com.li")
