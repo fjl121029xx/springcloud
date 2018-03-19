b1196e488de60e1fbc6092e0b71c9d07e2973ace
fjl121029xx <sishengqikuo_xx@163.com>
2018-03-19 08:07:46 +0800

    spring cloud 通过视同Eureka注册服务

--- src/main/java/com/li/springcloud/SpringcloudApplication.java
+++ src/main/java/com/li/springcloud/SpringcloudApplication.java
@@ -5,12 +5,17 @@ import com.li.springcloud.entity.Student;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.util.List;
 
 @SpringBootApplication
 @MapperScan("com.li")
+/**
+ * @author li
+ */
+@EnableEurekaClient
 public class SpringcloudApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/li/springcloud/controller/StudentController.java
+++ src/main/java/com/li/springcloud/controller/StudentController.java
@@ -2,7 +2,11 @@ package com.li.springcloud.controller;
 
 import com.li.springcloud.dao.StudentMapper;
 import com.li.springcloud.entity.Student;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
@@ -10,12 +14,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @Controller
 public class StudentController {
+
     @Autowired
     private RestTemplateBuilder builder;
+
+    @Autowired
+    @Qualifier("eurekaClient")
+    private EurekaClient eurekaClient;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     // 使用RestTemplateBuilder来实例化RestTemplate对象，spring默认已经注入了RestTemplateBuilder实例
     @Bean
@@ -38,4 +51,19 @@ public class StudentController {
         return sts;
     }
 
+    @GetMapping("/eureka-instance")
+    @ResponseBody
+    public String serviceUrl() {
+
+        InstanceInfo stores = eurekaClient.getNextServerFromEureka("STUDENT-SERVER", false);
+        return stores.getHomePageUrl();
+    }
+
+    @GetMapping("/eureka-instance-info")
+    @ResponseBody
+    public String shouInfo() {
+        //本来要写discoverclient
+
+        return "";
+    }
 }
